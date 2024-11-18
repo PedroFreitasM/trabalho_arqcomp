39cac593584ecb2e59ab467471b7132c9e137537
PedroFreitasM <130061255+PedroFreitasM@users.noreply.github.com>
2024-11-18 02:33:26 -0300

    Adição do criar_processos

--- structs.h
+++ structs.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <input_txt.h>
 
 #define DISCO 0
 #define FITA_MAG 1
@@ -12,6 +13,8 @@ typedef struct io{
 
 typedef struct Processo {
     int pid;
+    //Status e prioridade podem iniciar como 0 sempre (?)
+    //Esses dois podem ir mudando no round robin 
     int status;
     int prioridade;
 
@@ -35,3 +38,48 @@ typedef struct queue{
     queue_nodo *inicio;
     queue_nodo *fim;
 } queue;
+
+processo *criar_processo(char* linha){
+    printf("Novo processo: %s\n", linha);
+    int i = 0;
+    int pid = get_int(linha, &i);
+    printf("PID: %d\n", pid);
+    int tempo_chegada = get_int(linha, &i);
+    printf("Tempo de entrada: %d\n", tempo_chegada);
+    int t_cpu = get_int(linha, &i);
+    printf("Tempo de cpu: %d\n", t_cpu);
+    int quant_io = get_int(linha, &i);
+    printf("Quantia de IOs: %d\n", quant_io);
+
+    processo* p = (processo*)malloc(sizeof(processo));
+    if(!p){
+        printf("Falha na alocação do processo!\n");
+        return NULL;
+    }
+
+    p->pid = pid;
+    p->status = 0;
+    p->prioridade = 0;
+    p->tempo_entrada = tempo_chegada;
+    p->tempo_cpu = t_cpu;
+
+    p->tempo_usado = 0;
+    //Já que o processo é novo...
+
+    p->io_qtd = quant_io;
+
+    if(quant_io > 0){
+        p->ios = (io*)malloc(quant_io * sizeof(io));
+
+        for(int j=0; j < quant_io; j++){
+            int tipo_io = get_int(linha, &i);
+            int tempo_io = get_int(linha, &i);
+            printf("Tipo de IO: %d \n Tempo do IO: %d\n\n", tipo_io, tempo_io);
+            p->ios[j].tipo = tipo_io;
+            p->ios[j].tempo = tempo_io;
+        }
+    }
+    p->prox_io = 0;
+
+    return p;
+}
